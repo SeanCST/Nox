4a91e71a319e1eede82b5295836b546129126a4d
xuyongshi <heliocc@163.com>
2023-04-04 01:08:32 +0800

    init

---
+++ nox-bff/nox-bff-deploy/src/main/java/com/mall/noxbff/NoxBffDeployApplication.java
@@ -0,0 +1,13 @@
+package com.mall.noxbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoxBffDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoxBffDeployApplication.class, args);
+    }
+
+}
---
+++ nox-bff/nox-bff-web/src/main/java/com/mall/noxbff/web/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.mall.noxbff.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping
+    public String hello(){
+        return "HelloWorld!";
+    }
+}
