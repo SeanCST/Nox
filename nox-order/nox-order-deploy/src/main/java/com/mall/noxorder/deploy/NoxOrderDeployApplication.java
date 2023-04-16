974c3cecc98cb0c75a02ed4e33c0483f2671df44
xuyongshi <heliocc@163.com>
2023-04-16 21:28:40 +0800

    add nox-order

---
+++ nox-order/nox-order-deploy/src/main/java/com/mall/noxorder/deploy/NoxOrderDeployApplication.java
@@ -0,0 +1,13 @@
+package com.mall.noxorder.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoxOrderDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoxOrderDeployApplication.class, args);
+    }
+
+}
